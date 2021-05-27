dadb79b2feed73d8deda5bff548ce9a677d401bb
Fabian <fpinoperez@outlook.cl>
2021-05-27 19:21:08 -0400

    solucion a error al borrar usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :likes
-  has_many :friends
+  has_many :friends, dependent: :destroy
   scope :tweets_for_me, ->(user) { Tweet.where(user_id: user.friends.pluck(:friend_id).uniq) }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
